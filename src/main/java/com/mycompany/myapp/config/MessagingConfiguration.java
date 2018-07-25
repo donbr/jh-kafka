7360f51c4e019a814c0f6dc9d1133145c1f2c5bc
don-ibm <don.branson@ibm.com>
2018-07-24 23:18:14 -0500

    add Kafka Producer and Consumer functionality

--- src/main/java/com/mycompany/myapp/config/MessagingConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/MessagingConfiguration.java
@@ -3,6 +3,8 @@ package com.mycompany.myapp.config;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.mycompany.myapp.messaging.ConsumerChannel;
+import com.mycompany.myapp.messaging.ProducerChannel;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
@@ -18,19 +20,13 @@ import org.springframework.messaging.support.GenericMessage;
  * See http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
  * for the official Spring Cloud Stream documentation.
  */
-@EnableBinding(value = { Source.class })
+/**
+ * Configures Spring Cloud Stream support.
+ *
+ * See http://docs.spring.io/spring-cloud-stream/docs/current/reference/htmlsingle/
+ * for more information.
+ */
+@EnableBinding(value = {Source.class, ProducerChannel.class, ConsumerChannel.class})
 public class MessagingConfiguration {
 
-    /**
-     * This sends a test message at regular intervals.
-     *
-     * In order to see the test messages, you can use the Kafka command-line client:
-     * "./kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic topic-jhipster --from-beginning".
-     */
-    @Bean
-    @InboundChannelAdapter(value = Source.OUTPUT)
-    public MessageSource<String> timerMessageSource() {
-        return () -> new GenericMessage<>("Test message from JHipster sent at " +
-            new SimpleDateFormat().format(new Date()));
-    }
 }
--- src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/SecurityConfiguration.java
@@ -104,6 +104,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/greetings/**").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/**").authenticated()
